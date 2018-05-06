d192a81803b8f2ec1edf68105bb8ccd775ccd549
Danny Boehm <daniel.boehm728@myci.csuci.edu>
2018-05-05 20:30:37 -0700

    Updating Transisitions
    Screen now fades out when Play Menu is Clicked. Loads new level. Music also fades out to start the game.

--- Match/Assets/_Core/Scripts/MainMenu.cs
+++ Match/Assets/_Core/Scripts/MainMenu.cs
@@ -2,17 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MainMenu : MonoBehaviour {
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour
+{
 
-   
+    public Image Black;
+    public Animator anim;
 
-    public void PlayGame()
+    [SerializeField] private float delay = 1f;
+    public string level;
+
+    //function to be called on button click
+    public void LoadNextLevel(string level)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LevelLoad(level));
     }
 
-    void OnMouseOver()
+    //load level after one sceond delay
+    IEnumerator LevelLoad(string name)
+    {
+
+        StartCoroutine(Fading());
+        float elapsedTime = 0;
+        float currentVolume = AudioListener.volume;
+
+      
+        while (elapsedTime < delay)
+        {
+            elapsedTime += Time.deltaTime;
+            AudioListener.volume = Mathf.Lerp(currentVolume, 0, elapsedTime / delay);
+        }
+       
+
+        yield return null;
+    }
+
+    IEnumerator Fading()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => Black.color.a == 1);
+        SceneManager.LoadScene(level);
+    }
+
+    public void OnMouseOver()
     {
         FindObjectOfType<AudioM>().Play("HoverOver");
     }
+
 }
+
+
+
+
