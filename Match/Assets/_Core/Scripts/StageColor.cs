ee4922b3f3f36831879cf198c53da356bc2f69be
Danny Boehm <daniel.boehm728@myci.csuci.edu>
2018-05-05 18:16:26 -0700

    Updated Sound Effects
    Added the Background Music. Completed the Settings Volume options for BGM and SFX for Main Menu.

---
+++ Match/Assets/_Core/Scripts/StageColor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageColor : MonoBehaviour {
+
+
+    public SpriteRenderer stage;
+    Color original;
+
+    private void Awake()
+    {
+        original = stage.color;
+    }
+
+    public void OnMouseOver()
+    {
+        stage.color = new Color32(249, 58, 58, 163);
+    }
+
+    public void OnMouseEnter()
+    {
+        stage.color = new Color32(249, 58, 58, 163);
+    }
+
+    public void OnMouseExit()
+    {
+        stage.color = original;
+
+    }
+
+
+
+
+}
---
+++ Match/Assets/_Core/Scripts/VolumeAdjuster.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeAdjuster : MonoBehaviour {
+
+    public Slider volumeSlider;
+    public AudioSource volumeAudio;
+
+
+    public void OnValueChanged()
+    {
+        volumeAudio.volume = volumeSlider.value;
+    }
+
+}
